20f3da9da88af657cf4ba542b88edcd660931b31
Asif Ameen <apasifameen@gmail.com>
2025-07-12 14:37:45 +0530

    Apply edited version

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from './App'; // This will be your landing page
+import BlogPage from './BlogPage'; // This will be your new blog page
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="blogs" element={<BlogPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
